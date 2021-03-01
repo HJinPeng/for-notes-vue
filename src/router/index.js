ef26179c0bd18dd218faa4c664cc4e5392a18c92
jinpengh <jinpengh@digitalgd.com.cn>
2021-03-01 18:11:49 +0800

    feat: watch的笔记

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,11 @@ const routes = [
     component: Home
   },
   {
+    path: '/watch',
+    name: 'Watch',
+    component: () => import(/* webpackChunkName: "watch" */ '../views/watch/watch-base.vue')
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
---
+++ src/views/watch/watch-base.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="watch-base">
+    <p>
+      <span>watch的对象: </span>
+      <span>{{genderDict}}</span>
+    </p>
+    <p>
+      <span>改变的对象: </span>
+      <span>{{gender}}</span>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        genderDict: '1',
+        gender: ''
+      }
+    },
+    watch: {
+      genderDict: {
+        handler(newVal, oldVal){
+          console.log('监听genderDict',newVal, oldVal);
+          this.gender = newVal === '1' ? '男' : '女'
+        },
+        // immediate: true
+      }
+    }
+  }
+</script>
